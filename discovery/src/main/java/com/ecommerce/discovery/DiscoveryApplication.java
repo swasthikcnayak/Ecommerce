23007359694d6041685e74c3dfcd85c511ad12c1
Mayur Goyal <48891724+Mayur500@users.noreply.github.com>
2024-07-20 00:26:23 +0530

    add discovery service

---
+++ discovery/src/main/java/com/ecommerce/discovery/DiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
